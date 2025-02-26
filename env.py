be1e1303fdbb75f6a0870fe946c2a63273709e3f
hmoon96 <hlmooney98@gmail.com>
2025-02-26 16:53:36 +0700

    Altered Secret Key

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_gcCSHOb23Bxe@ep-sweet-recipe-a2obfb7d.eu-central-1.aws.neon.tech/stage_whoop_land_661690")
+os.environ.setdefault("SECRET_KEY", "V7oO+vGDQ4|4C7{0S(^IL$K~+")
