ce20ff7bb74bf671c3a64e64e08ea8b808c79d52
hmoon96 <hlmooney98@gmail.com>
2025-03-04 17:10:05 +0700

    enable serving of image files

--- env.py
+++ env.py
@@ -3,3 +3,5 @@ import os
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_gcCSHOb23Bxe@ep-sweet-recipe-a2obfb7d.eu-central-1.aws.neon.tech/stage_whoop_land_661690")
 os.environ.setdefault("SECRET_KEY", "V7oO+vGDQ4|4C7{0S(^IL$K~+")
+os.environ.setdefault(
+    "CLOUDINARY_URL", "cloudinary://191481669157289:CnNW4Qb5iRGSy25V8kzbKAB4jH8@dpviejnfa")
